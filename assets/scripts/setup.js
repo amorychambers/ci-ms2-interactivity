7cdca74e9cb73e9706cb2577ade36f3b727779c2
Amory <108595086+amorychambers@users.noreply.github.com>
2024-03-29 23:03:04 +0000

    load index.html into JSDOM successfully

--- assets/scripts/setup.js
+++ assets/scripts/setup.js
@@ -1,3 +1,9 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require('fs');
+const fileContents = fs.readFileSync('index.html', 'utf-8');
+const document = new JSDOM(fileContents).window.document;
+
 const game = {
     steamLibrary: [],
     randomGames: [],
@@ -6,32 +12,32 @@ const game = {
     currentScore: 0,
 };
 
-// document.getElementById('summon').addEventListener('click', getSteamLibrary);
+document.getElementById('summon').addEventListener('click', getSteamLibrary);
 
 
-// function getSteamLibrary() {
+function getSteamLibrary() {
 
-//         var baseURL = 'http://localhost:5500/getlibrary/?';
-//         // var userID = document.getElementById('userID').value;
-//         // Using static ID for testing
-//         var userID = '76561198033224422'
-//         var newURL = baseURL + userID;
+        var baseURL = 'http://localhost:5500/getlibrary/?';
+        // var userID = document.getElementById('userID').value;
+        // Using static ID for testing
+        var userID = '76561198033224422'
+        var newURL = baseURL + userID;
 
-//         var req = new XMLHttpRequest();
-//         req.open('GET', newURL, true);
-//         // This function isolates the games array from the Steam Web API response and assigns it to the steamLibrary key 
-//         req.addEventListener('load',function () {
-//             if (this.readyState == 4 && this.status == 200) {
-//                 const steamData = JSON.parse(req.responseText);
-//                 game.steamLibrary = steamData.response.games;
-//                 console.log(game.steamLibrary)
-//             } else {
-//                 console.log('Error type: ' + this.status)
-//             }});
-//             // Check Steam API status options for different incorrect data inputs later and account for them with alerts
+        var req = new XMLHttpRequest();
+        req.open('GET', newURL, true);
+        // This function isolates the games array from the Steam Web API response and assigns it to the steamLibrary key 
+        req.addEventListener('load',function () {
+            if (this.readyState == 4 && this.status == 200) {
+                const steamData = JSON.parse(req.responseText);
+                game.steamLibrary = steamData.response.games;
+                console.log(game.steamLibrary)
+            } else {
+                console.log('Error type: ' + this.status)
+            }});
+            // Check Steam API status options for different incorrect data inputs later and account for them with alerts
 
-//             req.send();
-//         };
+            req.send();
+        };
 
 module.exports = {game};
 
