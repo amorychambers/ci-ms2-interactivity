ace66ad3857805f0389d55176b0e7f5ccc78bc49
Amory <108595086+amorychambers@users.noreply.github.com>
2024-03-29 17:36:21 +0000

    fix error with CORS block on Steam Web API

--- assets/scripts/setup.js
+++ assets/scripts/setup.js
@@ -7,30 +7,37 @@ const game = {
 };
 
 
-// document.getElementById('summon').addEventListener('click', getSteamLibrary);
+document.getElementById('summon').addEventListener('click', getSteamLibrary);
 
 function getSteamLibrary() {
 
-        var baseURL = 'http://127.0.0.1:5500/getlibrary/?';
+        var baseURL = 'http://localhost:5500/getlibrary/?';
         var userID = document.getElementById('userID').value;
         var newURL = baseURL + userID;
 
         var req = new XMLHttpRequest();
         req.open('GET', newURL, true);
-        req.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                game.steamLibrary = xhttp.responseText;
-                console.log('Success!')
-            } else {
-                console.log('Error in request: ' + req.statusText)
-            };
+        // req.onreadystatechange = function () {
+        //     if (this.readyState == 4 && this.status == 200) {
+        //         game.steamLibrary = xhttp.responseText;
+        //         console.log('Success!')
+        //     } else {
+        //         console.log('Error in request')
+        //     };
+        req.addEventListener('load', function(){
+            if(req.status>= 200 && req.status<400){
+            var response = JSON.parse(req.responseText);
+            console.log(JSON.parse(req.responseText));
+            }
+            else {
+                console.log("Error in network request: " + req.statusText);
+            }
+        });
             // Check Steam API status options for different incorrect data inputs later and account for them with alerts
 
+            req.send();
         };
-        req.send();
-};
 
-getSteamLibrary();
+
 
 console.log('hello');
-
