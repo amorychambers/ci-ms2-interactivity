8935efd2bc67727d3962d91448362875eab670a5
Amory <108595086+amorychambers@users.noreply.github.com>
2024-04-03 18:06:17 +0100

    switch to developing game.js, create showComputerTurn function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,1 +1,13 @@
-
+function showComputerTurn(array) {
+    let thisTurn = [];
+    switch (game.currentScore) {
+        case 1:
+            thisTurn = array.slice(0, 4);
+            break;
+        case 2:
+            thisTurn = array.slice(0, 5);
+            break;
+        case 3:
+            thisTurn = array.slice(0, 6);
+    }
+}
