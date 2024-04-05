c25072ba8bbe056732a09ec97350c39d80e9e369
Amory <108595086+amorychambers@users.noreply.github.com>
2024-04-05 23:13:44 +0100

    add visual feedback and response message to contact form for when Promise returns error

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -9,13 +9,24 @@ function sendMail(form) {
         successMessage.style.display = 'none';
         successMessage.innerHTML = `<i class="fa-solid fa-circle-check fa-2xl mb-4" style="color: #0bda51;"></i><br>
         <p>Thanks for getting in touch! I'll try to get back to you at an ungodly hour and within a reasonable timescale for a person that is also facing down a neglected backlog.</p>`;
-        document.getElementById('contact-form').insertAdjacentElement('afterend', successMessage)
+        document.getElementById('contact-form').insertAdjacentElement('afterend', successMessage);
         $('#contact-form').fadeOut(1500);
         $('#contact-success').delay(1500).fadeIn(1000);
         $('#contact-success').addClass('center m-4');
     }), function(error){
-
-    }
+        let errorMessage = document.createElement('div');
+        errorMessage.setAttribute('id', 'contact-error')
+        errorMessage.style.display = 'none';
+        errorMessage.innerHTML = `<i class="fa-solid fa-circle-check fa-2xl mb-4" style="color: #0bda51;"></i><br>
+        <p>Oh dear. I'm afraid there was an error processing your response! Error type: ${error}</p>
+        <p>Here is the message you entered in case you'd like to save it somewhere and try again later!</p>
+        <label for='message'>Message</label>
+        <input type='textarea' name='message' value=${form.message.value}>`;
+        document.getElementById('contact-form').insertAdjacentElement('afterend', errorMessage);
+        $('#contact-form').fadeOut(1500);
+        $('#contact-error').delay(1500).fadeIn(1000);
+        $('#contact-error').addClass('center m-4');
+    };
     return false;
 };
 
