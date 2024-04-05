953ff3ad33e2330a7052ce384314d359b63c7139
Amory <108595086+amorychambers@users.noreply.github.com>
2024-04-05 22:56:42 +0100

    add visual feedback to contact form in email.js to inform user that submit event was successful and to wait for a success/failure response message

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -4,9 +4,22 @@ function sendMail(form) {
         "from_email": form.email.value,
         "message": form.message.value,
     }).then(function(response){
-
+        let successMessage = document.createElement('div');
+        successMessage.setAttribute('id', 'contact-success')
+        successMessage.style.display = 'none';
+        successMessage.innerHTML = `<i class="fa-solid fa-circle-check fa-2xl mb-4" style="color: #0bda51;"></i><br>
+        <p>Thanks for getting in touch! I'll try to get back to you at an ungodly hour and within a reasonable timescale for a person that is also facing down a neglected backlog.</p>`;
+        document.getElementById('contact-form').insertAdjacentElement('afterend', successMessage)
+        $('#contact-form').fadeOut(1500);
+        $('#contact-success').delay(1500).fadeIn(1000);
+        $('#contact-success').addClass('center m-4');
     }), function(error){
 
     }
     return false;
-}
+};
+
+$('#contact-form').on('submit', function(){
+    $('#contact-submit').attr('value', 'PLEASE WAIT...');
+    $('#contact-submit').addClass('active');
+})
