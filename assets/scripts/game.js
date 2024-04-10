a42cf0dae38a956fb34bb50185d515979397b7fd
Amory <108595086+amorychambers@users.noreply.github.com>
2024-04-10 18:40:03 +0100

    direct both functions fetching data to use the port provided by Heroku

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -297,7 +297,7 @@ function fetchAppNews() {
 
     return new Promise(function (resolve) {
 
-        var baseURL = 'http://localhost:5500/getnews/?';
+        var baseURL = `http://localhost:${process.env.port}/getlibrary/?`;
         var newsAppID = finalGame.appid;
         var newURL = baseURL + newsAppID;
 
