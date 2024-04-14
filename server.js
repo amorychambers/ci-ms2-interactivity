95903cd7f0972830c181c3f1afec4c24cf6da26a
Amory <108595086+amorychambers@users.noreply.github.com>
2024-04-14 21:50:21 +0100

    add path parameter to sendFile method

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/getnews', function(req, res) {
 });
 
 app.use((req, res, next) => {
-    res.status(404).sendFile('404.html')
+    res.status(404).sendFile(path.join(__dirname) + '/404.html')
   });
 
 app.listen(app.get('port'), function(){
